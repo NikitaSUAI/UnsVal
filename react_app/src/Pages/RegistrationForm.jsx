c158d3454ac6eda9c36dd42d49f12d61c8f7d77a
VladislavChepusov <vladislavchepusov@gmail.com>
2023-11-11 15:44:28 +0300

    result_11.11
    Авторизация
    регистрация
    форма ответов))))000)00))

--- react_app/src/Pages/RegistrationForm.jsx
+++ react_app/src/Pages/RegistrationForm.jsx
@@ -1,12 +1,19 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
+import axios from 'axios';
+import Config from '../config.js';
+import { useNavigate } from 'react-router-dom';
+import { setCookie } from '../Cook.js';
 import "../Styles/styles.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
-
 const RegistrationForm = () => {
+    const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    let navigate = useNavigate();
     const [showPassword, setShowPassword] = useState(false);
     const [credentials, setCredentials] = useState({ username: "", email: "", password: "", confirmPassword: "" });
-    const [errorMessage, setErrorMessage] = useState("");
+    const [errorMessage, setErrorMessage] = useState(null);
 
     const handleTogglePassword = () => {
         setShowPassword(!showPassword);
@@ -15,7 +22,7 @@ const RegistrationForm = () => {
     const handleChange = (event) => {
         const { name, value } = event.target;
         setCredentials(prevState => ({ ...prevState, [name]: value }));
-        setErrorMessage(""); // clear error message when changes occur
+        setErrorMessage(""); 
     };
 
     const handleSubmit = (event) => {
@@ -28,9 +35,33 @@ const RegistrationForm = () => {
             setErrorMessage("Password must be at least 6 characters long!");
             return;
         }
-        //Process the registration on server
-        console.log(credentials);
+        handleRegistration(event);
     };
+
+    const handleRegistration = async (event) => {
+        event.preventDefault();
+        const response = await fetch(Config.serverURL + 'api/registration/', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                username: credentials.username,
+                email: credentials.email,
+                password: credentials.password
+            }),
+        });
+        if (!response.ok) {
+            const data = await response.json();
+            setErrorMessage(JSON.stringify(data)); 
+            return; 
+        }
+        const data = await response.json();
+        // Successful registration
+        setCookie('token', data.key, 7);
+        navigate('/work');
+    };
+
 
     return (
         <div id="login-form" style={{ display: 'flex', justifyContent: 'center', flexDirection: 'column' }}>
@@ -38,11 +69,8 @@ const RegistrationForm = () => {
             <form onSubmit={handleSubmit}>
                 <label htmlFor="username">Username:</label>
                 <input type="text" id="username" name="username" className="input-field" onChange={handleChange} />
-
                 <label htmlFor="email">Email:</label>
-                <input type="email" id="email" name="email" className="input-field" onChxange={handleChange} />
-
-
+                <input type="email" id="email" name="email" className="input-field" onChange={handleChange} />
                 <label htmlFor="password">Password:</label>
                 <div className="password-input">
                     <input
@@ -79,9 +107,7 @@ const RegistrationForm = () => {
                 </div>
 
                 <p className="error-message" style={{ color: 'red' }}>{errorMessage}</p>
-
-                <input type="submit" value="Submit" />
-
+                <input type="submit" value="Submit" onClick={handleSubmit} />
                 <br></br><br></br>
                 <p className="login-link" style={{ textAlign: 'center' }}>
                     Already part of our platform?
@@ -91,5 +117,5 @@ const RegistrationForm = () => {
         </div>
     );
 };
-
 export default RegistrationForm;
+
--- react_app/src/Pages/WorkPage.jsx
+++ react_app/src/Pages/WorkPage.jsx
@@ -1,35 +1,69 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
+import axios from 'axios';
+import Config from '../config.js';
+import "../Styles/styles.css";
 
-const WorkPage = () => {
-  const [inputValue, setInputValue] = useState("");
+const AnswerForm = () => {
+  const [userid, setUserid] = useState("");
+  const [line, setLine] = useState("");
+  const [result, setResult] = useState("");
 
   const handleInputChange = (event) => {
-    setInputValue(event.target.value);
-  };
-
-  const handleGenerate = () => {
-    // Logic for generating something using inputValue
-    console.log(`Generating with input: ${inputValue}`);
+    setLine(event.target.value);
   };
 
   const handleLogout = () => {
-    // Logic for logging out
-    console.log("Logging out...");
+    document.cookie = 'token=; Max-Age=-99999999;'; // Удаляем токен из куки
+    console.log("Token removed from the cookie. Logging out...");
+  };
+
+  const handleSubmit = async () => {
+    const requestBody = {
+      userid: "СЮДА ВАШ АЙДИШНИК",
+      line: line
+    };
+
+    try {
+      const response = await fetch(Config.serverURL + 'api/answer/', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(requestBody)
+      });
+      const data = await response.json();
+      setResult(JSON.stringify(data)); // Отображение результата запроса
+    } catch (error) {
+
+      setResult('Ошибка при отправке запроса');
+    }
   };
 
   return (
     <div>
-      <header style={{ display: "flex", justifyContent: "flex-end", padding: "1em", backgroundColor: "skyblue" }}>
-   
-        <a href="/" className="btn btn-primary"> Logout </a>
+      <header class="p-3 bg-primary text-white">
+        <div class="container">
+          <div class="d-flex flex-wrap align-items- justify-content-end justify-content-lg-start"> 
+            <div className="text-end">
+              <a href="/" className="btn btn-outline-light" onClick={handleLogout}> Logout </a> 
+            </div>
+
+          </div>
+        </div>
       </header>
-      
       <main style={{ display: "flex", flexDirection: "column", alignItems: "center", marginTop: "2em" }}>
-        <input type="text" value={inputValue} onChange={handleInputChange} />
-        <button onClick={handleGenerate} style={{ marginTop: "1em" }}>Generate</button>
+        <div className="mb-3" style={{ width: "40%" }}>
+          <textarea type="text" className="form-control" value={line} onChange={handleInputChange} />
+        </div>
+        <div className="mb-3">
+          <button onClick={handleSubmit} className="btn btn-primary">Отправить</button>
+        </div>
+        <div className="mb-3">
+          <textarea value={result} readOnly className="form-control" rows="3" />
+        </div>
       </main>
     </div>
   );
 };
 
-export default WorkPage;
+export default AnswerForm;
